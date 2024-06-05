b2b7ceee3756b0c843c3c8104fa8ad36ed639294
gneiru <noelrohi59@gmail.com>
2024-06-05 17:09:39 +0800

    test npm package

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
+<script lang="ts">
+	import { add } from 'sk';
+
+	const asadasd = add(1, 100);
+</script>
+
+{asadasd}
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
